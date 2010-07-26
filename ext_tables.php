f297400d58b80d0a68af5590d58c8e5755d4cbfa
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-26 18:13:34 +0000

    Remove needless code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sexybookmarks/trunk@36073 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,9 +3,6 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key';
-
 // Static
 t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', 'Sexy-Bookmarks');
 
@@ -14,7 +11,7 @@ t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
 
-
+// ICON
 t3lib_extMgm::addPlugin(array(
 	'LLL:EXT:sexybookmarks/locallang_db.xml:tt_content.list_type_pi1',
 	$_EXTKEY . '_pi1',
