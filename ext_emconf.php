0d284b304384d1bdcd12422bdbd71ca02373caa1
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-21 21:07:17 +0000

    Add Google+ share-Button (#28458)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sexybookmarks/trunk@56538 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "sexybookmarks".
 #
-# Auto generated 27-11-2011 19:23
+# Auto generated 21-01-2012 22:06
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.1.3',
+	'version' => '1.1.4',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:48:{s:21:"ext_conf_template.txt";s:4:"fa24";s:12:"ext_icon.gif";s:4:"e371";s:17:"ext_localconf.php";s:4:"4950";s:14:"ext_tables.php";s:4:"cdb3";s:13:"locallang.xml";s:4:"4e76";s:16:"locallang_db.xml";s:4:"e605";s:12:"t3jquery.txt";s:4:"44a1";s:14:"doc/manual.sxw";s:4:"5942";s:44:"lib/class.tx_sexybookmarks_itemsProcFunc.php";s:4:"cc8f";s:43:"lib/class.tx_sexybookmarks_pagerenderer.php";s:4:"2310";s:15:"pi1/ce_icon.gif";s:4:"81e1";s:14:"pi1/ce_wiz.gif";s:4:"80ff";s:34:"pi1/class.tx_sexybookmarks_pi1.php";s:4:"4c5f";s:42:"pi1/class.tx_sexybookmarks_pi1_wizicon.php";s:4:"2d84";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flexform_ds.xml";s:4:"0dd2";s:17:"pi1/locallang.xml";s:4:"8e15";s:15:"pi2/ce_icon.gif";s:4:"34b6";s:14:"pi2/ce_wiz.gif";s:4:"1440";s:34:"pi2/class.tx_sexybookmarks_pi2.php";s:4:"c8fd";s:42:"pi2/class.tx_sexybookmarks_pi2_wizicon.php";s:4:"368d";s:13:"pi2/clear.gif";s:4:"cc11";s:19:"pi2/flexform_ds.xml";s:4:"f373";s:17:"pi2/locallang.xml";s:4:"8e15";s:23:"res/tx_sexybookmarks.js";s:4:"2ab0";s:17:"res/css/style.css";s:4:"b45a";s:24:"res/diaspora/favicon.ico";s:4:"8a69";s:26:"res/diaspora/share-de.html";s:4:"5358";s:25:"res/images/new-sprite.xcf";s:4:"df61";s:26:"res/images/sexy-sprite.png";s:4:"8f0d";s:34:"res/images/share-caring-hearts.png";s:4:"1963";s:27:"res/images/share-caring.png";s:4:"1ab2";s:26:"res/images/share-enjoy.png";s:4:"35dc";s:27:"res/images/share-german.png";s:4:"0ca6";s:30:"res/images/share-knowledge.png";s:4:"fb86";s:32:"res/images/share-love-hearts.png";s:4:"0845";s:25:"res/images/share-sexy.png";s:4:"f324";s:27:"res/images/share-wealth.png";s:4:"4569";s:33:"res/jquery/js/jquery-1.7.1.min.js";s:4:"ddb8";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:30:"res/jquery/js/sexybookmarks.js";s:4:"b181";s:20:"static/constants.txt";s:4:"447e";s:16:"static/setup.txt";s:4:"838d";s:29:"static/followus/constants.txt";s:4:"9808";s:25:"static/followus/setup.txt";s:4:"2e23";s:45:"widgets/class.tx_sexybookmarks_getwidgets.php";s:4:"78d0";s:62:"widgets/sexybookmarks/class.tx_sexybookmarks_sexybookmarks.php";s:4:"ebe1";s:35:"widgets/sexybookmarks/locallang.xml";s:4:"0116";}',
+	'_md5_values_when_last_written' => 'a:48:{s:21:"ext_conf_template.txt";s:4:"fa24";s:12:"ext_icon.gif";s:4:"e371";s:17:"ext_localconf.php";s:4:"4950";s:14:"ext_tables.php";s:4:"cdb3";s:13:"locallang.xml";s:4:"4e76";s:16:"locallang_db.xml";s:4:"e605";s:12:"t3jquery.txt";s:4:"44a1";s:14:"doc/manual.sxw";s:4:"f6f0";s:44:"lib/class.tx_sexybookmarks_itemsProcFunc.php";s:4:"cc8f";s:43:"lib/class.tx_sexybookmarks_pagerenderer.php";s:4:"2310";s:15:"pi1/ce_icon.gif";s:4:"81e1";s:14:"pi1/ce_wiz.gif";s:4:"80ff";s:34:"pi1/class.tx_sexybookmarks_pi1.php";s:4:"4c5f";s:42:"pi1/class.tx_sexybookmarks_pi1_wizicon.php";s:4:"2d84";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flexform_ds.xml";s:4:"0dd2";s:17:"pi1/locallang.xml";s:4:"8e15";s:15:"pi2/ce_icon.gif";s:4:"34b6";s:14:"pi2/ce_wiz.gif";s:4:"1440";s:34:"pi2/class.tx_sexybookmarks_pi2.php";s:4:"c8fd";s:42:"pi2/class.tx_sexybookmarks_pi2_wizicon.php";s:4:"368d";s:13:"pi2/clear.gif";s:4:"cc11";s:19:"pi2/flexform_ds.xml";s:4:"f373";s:17:"pi2/locallang.xml";s:4:"8e15";s:23:"res/tx_sexybookmarks.js";s:4:"2ab0";s:17:"res/css/style.css";s:4:"b45a";s:24:"res/diaspora/favicon.ico";s:4:"8a69";s:26:"res/diaspora/share-de.html";s:4:"5358";s:25:"res/images/new-sprite.xcf";s:4:"df61";s:26:"res/images/sexy-sprite.png";s:4:"8f0d";s:34:"res/images/share-caring-hearts.png";s:4:"1963";s:27:"res/images/share-caring.png";s:4:"1ab2";s:26:"res/images/share-enjoy.png";s:4:"35dc";s:27:"res/images/share-german.png";s:4:"0ca6";s:30:"res/images/share-knowledge.png";s:4:"fb86";s:32:"res/images/share-love-hearts.png";s:4:"0845";s:25:"res/images/share-sexy.png";s:4:"f324";s:27:"res/images/share-wealth.png";s:4:"4569";s:33:"res/jquery/js/jquery-1.7.1.min.js";s:4:"ddb8";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:30:"res/jquery/js/sexybookmarks.js";s:4:"b181";s:20:"static/constants.txt";s:4:"911e";s:16:"static/setup.txt";s:4:"0556";s:29:"static/followus/constants.txt";s:4:"9808";s:25:"static/followus/setup.txt";s:4:"2e23";s:45:"widgets/class.tx_sexybookmarks_getwidgets.php";s:4:"78d0";s:62:"widgets/sexybookmarks/class.tx_sexybookmarks_sexybookmarks.php";s:4:"ebe1";s:35:"widgets/sexybookmarks/locallang.xml";s:4:"0116";}',
 );
 
 ?>
